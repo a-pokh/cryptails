618a1abb5a3724e8a726be6b9711b2c4d4d1b352
Anton Pokhylenko <anton.pokhilenko@gmail.com>
2021-12-16 23:02:23 +0200

    Update list scripts

--- app/mp-list.js
+++ app/mp-list.js
@@ -17,16 +17,29 @@ const program = new anchor.Program(idl, programId);
 (async() => {
   const provider = anchor.Provider.local();
 
+  // get all token accounts in wallet by token program id
+  const { value: walletTokenAccounts } = await provider.connection.getTokenAccountsByOwner(
+    provider.wallet.publicKey, 
+    { programId: spl.TOKEN_PROGRAM_ID }
+  )
+
   const programAccounts = await provider.connection.getProgramAccounts(programId)
 
   // result list
   let listToShow = []
 
+  // get list of all wallet token pubkes strings
+  const walletTokenPubkeys = walletTokenAccounts.map(({pubkey}) => pubkey.toString())
+
   for(let programAccount of programAccounts) {
     // fetch specific cryptail account
     const cryptailAcc = await program.account.cryptail.fetch(programAccount.pubkey);
 
-    listToShow.push(cryptailAcc)
+    // TODO: find out if such chek is secure
+    // if this cryptail account belongs to user's wallet add it to result
+    if(walletTokenPubkeys.includes(cryptailAcc.tokenAccount.toString())) {
+      listToShow.push(cryptailAcc)
+    }
   }
 
   for(let tokenAccount of listToShow) {
